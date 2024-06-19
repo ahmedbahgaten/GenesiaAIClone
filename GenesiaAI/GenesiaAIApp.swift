1a2cd29fa3a1e417d43b591bd5e62cfca014b139
ahmedbahgaten <ahmed.nabih@flairstech.com>
2024-06-19 17:29:53 +0300

    Initial Commit

---
+++ GenesiaAI/GenesiaAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenesiaAIApp.swift
+//  GenesiaAI
+//
+//  Created by Ahmed Bahgat on 19/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct GenesiaAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
